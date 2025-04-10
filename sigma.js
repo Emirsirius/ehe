19ed9aae6790fd75e3150718b877a6a79e5d60e0
emiliemilli <emilie.milli@skole.arendal.kommune.no>
2025-04-10 10:33:43 +0200

    sigma

---
+++ sigma.js
@@ -0,0 +1,12 @@
+// sigma.js
+// A simple JavaScript file
+
+console.log("Hawk tuah!");
+
+// Example function
+function add(a, b) {
+    return a + b;
+}
+
+// Test the function
+console.log("Sum of 2 and 5 is:", add(2, 5));
